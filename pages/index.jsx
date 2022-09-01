190691de2f1175d5b52cb15d445c051d52ebac21
delasimensah <mensadelasi@gmail.com>
2022-09-01 14:19:01 +0000

    finished site

---
+++ pages/index.jsx
@@ -0,0 +1,54 @@
+import Head from "next/head";
+
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "../components";
+import styles from "../styles/style";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Hoobank</title>
+      </Head>
+
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats />
+            <Business />
+            <Billing />
+            <CardDeal />
+            <Testimonials />
+            <Clients />
+            <CTA />
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
